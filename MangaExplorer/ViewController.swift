1fd3943301522ffcdb3cbdbce34b090b5f96429c
Sanjib Ahmad <sanjib@sanjib.org>
2015-09-05 01:31:37 +0600

    Initial Commit

---
+++ MangaExplorer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MangaExplorer
+//
+//  Created by Sanjib Ahmad on 9/5/15.
+//  Copyright (c) 2015 Object Coder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
