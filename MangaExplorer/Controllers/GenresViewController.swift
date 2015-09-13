09aff922597bbaa6b9026b09518678ff574a33f5
Sanjib Ahmad <sanjib@sanjib.org>
2015-09-13 06:11:01 +0600

    Add genres view controller

--- MangaExplorer/Controllers/GenresViewController.swift
+++ MangaExplorer/Controllers/GenresViewController.swift
@@ -7,13 +7,39 @@
 //
 
 import UIKit
+import CoreData
 
-class GenresViewController: UIViewController {
+class GenresViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    private var allGenres = [String]()
 
+    private let genreImages = [
+        "action": UIImage(named: "action"),
+        "adventure": UIImage(named: "adventure"),
+        "comedy": UIImage(named: "comedy"),
+        "drama": UIImage(named: "drama"),
+        "fantasy": UIImage(named: "fantasy"),
+        "horror": UIImage(named: "horror"),
+        "magic": UIImage(named: "magic"),
+        "mystery": UIImage(named: "mystery"),
+        "psychological": UIImage(named: "psychological"),
+        "romance": UIImage(named: "romance"),
+        "science fiction": UIImage(named: "science fiction"),
+        "slice of life": UIImage(named: "slice of life"),
+        "supernatural": UIImage(named: "supernatural"),
+        "thriller": UIImage(named: "thriller"),
+        "tournament": UIImage(named: "tournament"),
+        "unknown": UIImage(named: "genres")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
 
-        // Do any additional setup after loading the view.
+        // CoreData
+        allGenres = fetchAllGenres()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +47,64 @@ class GenresViewController: UIViewController {
         println("didReceiveMemoryWarning: GenresViewController")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: - CoreData
+    
+    var sharedContext: NSManagedObjectContext {
+        return CoreDataStackManager.sharedInstance.managedObjectContext!
     }
-    */
+    
+    func fetchAllGenres() -> [String] {
+        let fetchRequest = NSFetchRequest()
+        fetchRequest.entity = NSEntityDescription.entityForName("Genre", inManagedObjectContext: sharedContext)
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
+        fetchRequest.resultType = NSFetchRequestResultType.DictionaryResultType
+        fetchRequest.returnsDistinctResults = true
+        fetchRequest.propertiesToFetch = ["name"]
+        
+        var error: NSError? = nil
+        var results = sharedContext.executeFetchRequest(fetchRequest, error: &error)
+        if let error = error {
+            return [String]()
+        }
 
+        var genres = [String]()
+        for genre in results as! [NSDictionary] {
+            if let genreName = genre["name"] as? String {
+                genres.append(genreName)
+            }
+        }
+        return genres
+    }
+    
+    // MARK: - TableView delegates & data source
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allGenres.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("GenreCell", forIndexPath: indexPath) as! GenreTableViewCell
+        configureCell(cell, atIndexPath: indexPath)
+        return cell
+    }
+    
+    // MARK: - Configure cell
+    
+    func configureCell(cell: GenreTableViewCell, atIndexPath indexPath: NSIndexPath) {
+        let genreName = allGenres[indexPath.row]
+        
+        if let genreImage = genreImages[genreName] {
+            cell.genreImageView.image = genreImage?.imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate)
+        } else {
+            if let genreImage = genreImages["unknown"] {
+                cell.genreImageView.image = genreImage?.imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate)
+            }
+        }
+        
+        cell.genreLabel.text = genreName.capitalizedString
+    }
 }
---
+++ MangaExplorer/Views/GenreTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  GenreTableViewCell.swift
+//  MangaExplorer
+//
+//  Created by Sanjib Ahmad on 9/13/15.
+//  Copyright (c) 2015 Object Coder. All rights reserved.
+//
+
+import UIKit
+
+class GenreTableViewCell: UITableViewCell {
+    @IBOutlet weak var genreImageView: UIImageView!
+    @IBOutlet weak var genreLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        genreImageView.contentMode = UIViewContentMode.ScaleAspectFit
+        genreImageView.clipsToBounds = true
+        genreImageView.tintColor = UIColor.lightGrayColor()
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
